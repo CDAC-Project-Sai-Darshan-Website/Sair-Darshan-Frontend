50dc94f156b3cc5296e8d1b6bfb51a751485127e
shubhammedewar <shubhammedewar@gmail.com>
2026-01-24 23:18:43 +0530

    uploading empty react project

---
+++ Sai_Darshan_Frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+function App() {
+
+  return (
+    <>
+      <p className="read-the-docs">
+        SAI DARSHAN
+      </p>
+    </>
+  )
+}
+
+export default App
